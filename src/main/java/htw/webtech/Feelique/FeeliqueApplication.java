35287b7f0a76bc31c84cb2def5adf5c9156673ad
yyyyasmin <yasmin.yelgin@student.htw-berlin.de>
2025-10-14 23:00:58 +0200

    Initial Commit

---
+++ src/main/java/htw/webtech/Feelique/FeeliqueApplication.java
@@ -0,0 +1,13 @@
+package htw.webtech.Feelique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeliqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeliqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/webtech/Feelique/FeeliqueApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.webtech.Feelique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeliqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
