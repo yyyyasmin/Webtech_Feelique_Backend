2e909ae6bb52ea8c8bb3ed67ae849d50d6ce94ba
mar3a-sch <maria.schulz@student.htw-berlin.de>
2025-11-21 10:44:22 +0100

    dockerfile 2

--- src/test/java/htw/webtech/Feelique/FeeliqueApplicationTests.java
+++ src/test/java/htw/webtech/Feelique/FeeliqueApplicationTests.java
@@ -2,8 +2,10 @@ package htw.webtech.Feelique;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class FeeliqueApplicationTests {
 
+    @Test
+    void contextLoads() {
+    }
 }
