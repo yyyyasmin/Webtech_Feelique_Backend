3efe7944d78cad0d02e417771239830a492b19b2
yyyyasmin <yasmin.yelgin@student.htw-berlin.de>
2026-01-13 15:52:43 +0100

    Revert "Authentifizierung"
    This reverts commit 3c9203cd94bbefbb8bfcbd6180f9207d5481cf69.

--- src/main/java/htw/webtech/Feelique/auth/AuthTokenRepository.java
+++
@@ -1,6 +0,0 @@
-package htw.webtech.Feelique.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthTokenRepository extends JpaRepository<AuthToken, String> {
-}
--- src/main/java/htw/webtech/Feelique/auth/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package htw.webtech.Feelique.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
